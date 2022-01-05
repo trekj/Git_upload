563e1737c20e06ceae7522289a160da58b0ad98e
JamesT <jtagapan@gmail.com>
2022-01-05 17:35:11 -0600

    added some codes

--- a_definitive_buy.py
+++ a_definitive_buy.py
@@ -5,6 +5,9 @@
 # The first message should state the cost of the transaction.
 # THe second message should state that the payment has been processed.
 # YOUR CODE HERE
+def process_payment():
+    print("the total cost of this transaction will be 75 cents")
+    print("Payment has been processed")
 
 
 # Call the function to run the code in the function.
