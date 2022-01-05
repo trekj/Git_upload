d6d9dea3cbfc30e19f2beb6106e748a7ecf60090
James Tagapan <93211640+trekj@users.noreply.github.com>
2022-01-05 17:32:20 -0600

    Add files via upload

---
+++ returned_goods.py
@@ -0,0 +1,17 @@
+"""Returned Goods."""
+
+
+# @TODO: Define a new function called process_claims
+# Inside of the function:
+# Create a variable called `total_claims`, that is the sum of all claims
+# Calculate a total payout, which is 30% of total_claims:
+# Return only the total_payout amount
+
+
+# @TODO Paste the list of weekly claims:
+weekly_claims = [5000, 1000, 8000, 10000, 3000, 3500]
+
+# What's the total insurance payout?
+# Use the print() statement to print the returned value from the function.
+# @TODO: Call the function using `weekly_claims` as the function argument
+# YOUR CODE HERE!
