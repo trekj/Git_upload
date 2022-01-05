398f9535c66f946a5edbc153d24ebfc0edd3e247
James Tagapan <93211640+trekj@users.noreply.github.com>
2022-01-05 12:43:08 -0600

    Add files via upload

--- nested_conditionals.py
+++ nested_conditionals.py
@@ -1,15 +1,24 @@
 """Nested Conditionals."""
 
-# @TODO: Create variables with the initial ad price and company type
-# YOUR CODE HERE!
+# Create variables with the initial ad price and company type
+ad_price = 50
+company_type = "Startup"
 
-# @TODO: Convert the following decision logic into valid Python code.
-# if the ad price is affordable (less than 20):
-#     if the company is a startup:
-#         print that the expected profit is 500
-#     elif the company is existing:
-#         print that the expected profit is 100
-#     else:
-#         print that the company type is not specified
-# else:
-#     print that the ad price is too expensive
+# Create a nested conditional logic statement to determine
+# the company's expected profit given the following conditions:
+# The code should first determine if the ad price is affordable or too expensive.
+# Any ad price under $20 can be considered affordable,
+# but anything else should be considered too expensive.
+# If the ad price is under $20, then make a decision based on the company type.
+# If the company is a "Startup", then print an expected profit of $500
+# If the company is "Existing", then print an expected profit of $100
+# Else, print that the company type was not specified.
+if ad_price < 20:
+    if company_type == "Startup":
+        print(f"Expected profit is ${500}")
+    elif company_type == "Existing":
+        print(f"Expected profit is ${100}")
+    else:
+        print("Company type is not specified.")
+else:
+    print("Advertising rates are currently too expensive")
