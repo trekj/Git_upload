966cb0278c4157bbc974f27f6c50bc2a453dea00
James Tagapan <93211640+trekj@users.noreply.github.com>
2022-01-05 15:57:15 -0600

    Add files via upload

---
+++ a_definitive_buy.py
@@ -0,0 +1,11 @@
+"""A Definitive Buy."""
+
+
+# @TODO: Create a function that prints two messages.
+# The first message should state the cost of the transaction.
+# THe second message should state that the payment has been processed.
+# YOUR CODE HERE
+
+
+# Call the function to run the code in the function.
+process_payment()
