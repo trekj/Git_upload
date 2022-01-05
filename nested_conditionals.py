009ad84e9b3c4247c036af2b9ae193a8af109d29
James Tagapan <93211640+trekj@users.noreply.github.com>
2022-01-05 12:07:57 -0600

    Add files via upload

---
+++ nested_conditionals.py
@@ -0,0 +1,15 @@
+"""Nested Conditionals."""
+
+# @TODO: Create variables with the initial ad price and company type
+# YOUR CODE HERE!
+
+# @TODO: Convert the following decision logic into valid Python code.
+# if the ad price is affordable (less than 20):
+#     if the company is a startup:
+#         print that the expected profit is 500
+#     elif the company is existing:
+#         print that the expected profit is 100
+#     else:
+#         print that the company type is not specified
+# else:
+#     print that the ad price is too expensive
