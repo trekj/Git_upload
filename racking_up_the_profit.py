80167d7ba2fa8838589478c0b4b985ef0224bcef
James Tagapan <93211640+trekj@users.noreply.github.com>
2022-01-06 10:38:58 -0600

    Add files via upload

---
+++ racking_up_the_profit.py
@@ -0,0 +1,16 @@
+"""Racking up the profit."""
+
+# Suppose each stock price is the same
+stock_price = 30
+
+# But that dividend yields vary across five stocks in your portfolio
+dividend_yields = [0.035, 0.040, 0.072, 0.012, 0.052]
+
+# @TODO: Create a variable to hold tallied dividend income
+# YOUR CODE HERE!
+
+# @TODO: Create a for loop to calculated and add up all the dividend income
+# YOUR CODE HERE!
+
+# @TODO: Once it's all done, print the dividend income for the entire portfolio
+# YOUR CODE HERE!
