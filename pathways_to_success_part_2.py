87b6efafbce4c5c87942a05f2ce5a2b090487565
James Tagapan <93211640+trekj@users.noreply.github.com>
2022-01-07 11:35:18 -0600

    Add files via upload

---
+++ pathways_to_success_part_2.py
@@ -0,0 +1,23 @@
+"""Pathways to Success Part 2."""
+# @TODO: Import the csv library
+# YOUR CODE HERE!
+from pathlib import Path
+
+# @TODO: Create a path to the csv file called `quarterly_data.csv`
+# YOUR CODE HERE!
+
+# @TODO: Open the csv path, read the data, and print each row
+# YOUR CODE HERE!
+
+"""Extension.
+
+This is an optional extension to the activity.
+
+Create a variable above the `for` loop named `Counter`
+and set it equal to zero. Then, every time a new row
+is read within the `for` loop, add a value of 1 to
+this variable.
+"""
+
+# @TODO: Add a row counter to the CSV data.
+# YOUR CODE HERE!
